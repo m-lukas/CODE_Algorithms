c19b8b24f3f3488de423bc16a173c5b5d180050d
m-lukas <mail@lukasmueller.de>
2019-11-24 11:17:44 +0200

    Add bubble sort implementation

---
+++ bubble_sort/main/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+	array := []int{3, 2, 1, 20, 5, 6, 42, -2, 4, 3}
+	sort(array)
+	fmt.Println(array)
+}
+
+func sort(array []int) {
+	sorted := false
+
+	for !sorted {
+		withoutSwap := true
+
+		for i := 0; i < len(array)-1; i++ {
+
+			if array[i] > array[i+1] {
+				swap(array, i, i+1)
+				withoutSwap = false
+			}
+		}
+
+		if withoutSwap == true {
+			sorted = true
+		}
+	}
+}
+
+func swap(array []int, first int, second int) {
+	if array[first] == array[second] {
+		return
+	}
+
+	swap := array[first]
+	array[first] = array[second]
+	array[second] = swap
+}
